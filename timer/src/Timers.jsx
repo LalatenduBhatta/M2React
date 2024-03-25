a73436fe7f36e0ed9771299fe37339e4fd10937c
LalatenduBhatta <lalatendubhatta@gmail.com>
2024-03-25 17:39:49 +0530

    timer logic1

--- timer/src/Timers.jsx
+++ timer/src/Timers.jsx
@@ -13,19 +13,23 @@ function Timers() {
         }
         return () => clearInterval(timer)
     }, [milliseconds])
+    let milli_sec = Math.trunc((milliseconds % 1000) / 10)
+    let seconds = Math.trunc(milliseconds / 1000) % 60
+    let minutes = Math.trunc(milliseconds / 60000) % 60
+    let hours = Math.trunc(milliseconds / (60000 * 60)) % 24
     return (
         <>
             <h1>Timers</h1>
             <div className="clock" style={{ color: "black" }}>
-                {milliseconds}
+                {hours}:{minutes}:{seconds}:{milli_sec}
             </div>
             <div className="time-buttons">
                 <button onClick={() => dispatch({ type: "1min" })}>1Min</button>
-                <button>5Min</button>
-                <button>10Min</button>
-                <button>15Min</button>
-                <button>30Min</button>
-                <button>1hr</button>
+                <button onClick={() => dispatch({ type: "5min" })}>5Min</button>
+                <button onClick={() => dispatch({ type: "10min" })}>10Min</button>
+                <button onClick={() => dispatch({ type: "15min" })}>15Min</button>
+                <button onClick={() => dispatch({ type: "30min" })}>30Min</button>
+                <button onClick={() => dispatch({ type: "1hr" })}>1hr</button>
             </div>
             <div className="inputs">
                 <label htmlFor="hours">Hours</label>
--- timer/src/reducers/timeReducer.js
+++ timer/src/reducers/timeReducer.js
@@ -4,6 +4,16 @@ export const timeReducer = (state = initialValue, action) => {
     switch (action.type) {
         case "1min":
             return 1000 * 60;
+        case "5min":
+            return 1000 * 60 * 5;
+        case "10min":
+            return 1000 * 60 * 10;
+        case "15min":
+            return 1000 * 60 * 15;
+        case "30min":
+            return 1000 * 60 * 30;
+        case "1hr":
+            return 1000 * 60 * 60;
         case "startTimer":
             return state - 10
         default:
