65baa3644db13cc5bc73414a0d8fb873cee8c087
LalatenduBhatta <lalatendubhatta@gmail.com>
2024-03-30 16:54:33 +0530

    e com

--- weatherappreact/src/App.jsx
+++ weatherappreact/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <div className=' bg-sky-800 text-slate-50 w-60 h-40'>TAILWIND</div>
+
+      <button className='p-20 m-12 border-2 border-rose-500 bg-lime-500'>HELLO</button>
+    </>
   )
 }
 
